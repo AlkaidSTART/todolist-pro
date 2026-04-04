186b6150121e97e269ae00b03bf830cae8093309
AlkaidSTART <2595006848@qq.com>
2026-04-04 13:29:38 +0800

    chore: add GitHub Actions workflow for auto deploy to gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'dist',
+  basePath: '/todolist-pro',
+  assetPrefix: '/todolist-pro/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
