6b9668e6602caad9afae24fbe7b5bc4763c1c15d
AlkaidSTART <2595006848@qq.com>
2026-04-04 16:54:42 +0800

    fix: 移除distDir配置以简化构建设置

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  distDir: "dist",
   basePath: isProd ? "/todolist-pro" : "",
   assetPrefix: isProd ? "/todolist-pro/" : "",
   images: {
